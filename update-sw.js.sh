2efa58e2023c956baf67dde12c8a1e32e03dcd17
marmooo <marmooo@users.noreply.github.com>
2021-06-07 00:05:27 +0900

    add

---
+++ update-sw.js.sh
@@ -0,0 +1,12 @@
+timestamp=$(date '+%Y-%m-%d %H:%M')
+keywordFrom="^var CACHE_NAME.*$"
+keywordTo="var CACHE_NAME = '$timestamp';"
+replace="sd -f m \"$keywordFrom\" \"$keywordTo\" src/sw.js"
+basedir=`pwd`
+while read line
+do
+  echo $replace
+  cd ../$line && eval $replace
+done < all.lst
+cd $basedir
+
