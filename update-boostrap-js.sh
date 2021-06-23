ee9ad5eb2ef7761a12e49115a933c9a69afde6dc
marmooo <marmooo@users.noreply.github.com>
2021-06-24 07:35:16 +0900

    update

--- update-boostrap-js.sh
+++ update-boostrap-js.sh
@@ -1,5 +1,5 @@
 keywordFrom='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>'
-keywordTo='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>'
+keywordTo='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>'
 
 replace="sd -s '$keywordFrom' '$keywordTo' \$(fd --type file -e html . src)"
 basedir=`pwd`
