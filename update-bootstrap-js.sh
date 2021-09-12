8d6e6059661126e063bbf3af612d05b64020fee7
marmooo <marmooo@users.noreply.github.com>
2021-09-12 10:38:03 +0900

    bump bootstrap from 5.1.0 to 5.1.1

--- update-bootstrap-js.sh
+++ update-bootstrap-js.sh
@@ -1,5 +1,5 @@
-keywordFrom='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>'
-keywordTo='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>'
+keywordFrom='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>'
+keywordTo='<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>'
 
 replace="sd -s '$keywordFrom' '$keywordTo' \$(fd --type file -e html . src)"
 basedir=`pwd`
--- update-bootstrap.sh
+++ update-bootstrap.sh
@@ -3,3 +3,4 @@ bash update-bootstrap-js.sh
 bash update-bootstrap-sw.js.sh
 bash update-sw.js.sh
 bash build.sh
+bash git-push.sh "bump bootstrap from 5.1.0 to 5.1.1"
