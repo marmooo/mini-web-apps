50c37737d371c6deeaad69442ccbc5bbb11f5f8c
marmooo <marmooo@users.noreply.github.com>
2021-11-18 09:31:40 +0900

    add graded-vocab-ja, graded-idioms-ja

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 basedir=`pwd`
 
 cd ../kanji-dict && node build.js && cd $basedir
+cd ../graded-vocab-ja && deno run --allow-read --allow-write build.js && cd $basedir
+cd ../graded-idioms-ja && deno run --allow-read --allow-write build.js && cd $basedir
 cd ../homonym-ja && deno run --allow-read --allow-write build.js && cd $basedir
 cd ../spelling-variants-ja && deno run --allow-read --allow-write build.js && cd $basedir
 cd ../shogi-beginners && hugo --minify && cd $basedir
