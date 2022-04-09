02376b1c167d87f6c4d772b0d89178fbbf5a482b
marmooo <marmooo@users.noreply.github.com>
2022-04-09 11:20:27 +0900

    add tfjs script

---
+++ update-tfjs.sh
@@ -0,0 +1,17 @@
+keywordFrom='https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.9.0/dist/tf.min.js';
+keywordTo='https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.15.0/dist/tf.min.js';
+
+replace="sd -s '$keywordFrom' '$keywordTo' \$(fdfind --type file -e js . src)"
+basedir=`pwd`
+while read line
+do
+  cd ../$line
+  eval $replace
+done < tfjs.lst
+cd $basedir
+
+
+
+bash update-sw.js.sh tfjs.lst
+bash build.sh tfjs.lst
+bash git-push.sh tfjs.lst "bump tfjs from 3.9.0 to 3.15.0"
