f954079f876756fead8d9a66adcfcb36b50cdec4
marmooo <marmooo@users.noreply.github.com>
2022-05-01 08:37:07 +0900

    bump tfjs from 3.15.0 to 3.16.0

--- update-tfjs.sh
+++ update-tfjs.sh
@@ -1,5 +1,5 @@
-keywordFrom='https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.9.0/dist/tf.min.js';
-keywordTo='https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.15.0/dist/tf.min.js';
+keywordFrom='https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.15.0/dist/tf.min.js';
+keywordTo='https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.16.0/dist/tf.min.js';
 
 replace="sd -s '$keywordFrom' '$keywordTo' \$(fdfind --type file -e js . src)"
 basedir=`pwd`
@@ -14,4 +14,4 @@ cd $basedir
 
 bash update-sw.js.sh tfjs.lst
 bash build.sh tfjs.lst
-bash git-push.sh tfjs.lst "bump tfjs from 3.9.0 to 3.15.0"
+bash git-push.sh tfjs.lst "bump tfjs from 3.15.0 to 3.16.0"
