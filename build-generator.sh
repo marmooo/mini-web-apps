cd9c5cddf81abf8ffc9595cc6a1b63ac0af4fe41
marmooo <marmooo@users.noreply.github.com>
2022-06-30 08:58:16 +0900

    fix

--- build-generator.sh
+++ build-generator.sh
@@ -5,5 +5,4 @@ cd ../graded-vocab-ja && deno run --allow-read --allow-write build.js && cd $bas
 cd ../graded-idioms-ja && deno run --allow-read --allow-write build.js && cd $basedir
 cd ../homonym-ja && deno run --allow-read --allow-write build.js && cd $basedir
 cd ../spelling-variants-ja && deno run --allow-read --allow-write build.js && cd $basedir
-cd ../shogi-beginners && hugo --minify && cd $basedir
-cd ../photo-scanner && bash build.sh && cd $basedir
+cd ../shogi-beginners && bash build.sh && cd $basedir
