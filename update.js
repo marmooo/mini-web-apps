0579709fba11e5c2c0deb5096a5419f43d1992a2
marmooo <marmooo@users.noreply.github.com>
2022-09-11 09:00:53 +0900

    bump bootstrap from 5.2.0 to 5.2.1

--- update.js
+++ update.js
@@ -81,9 +81,9 @@ async function updateTfjs(repoList) {
 
 async function updateBootstrapJs(repoList) {
   const from =
-    '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>';
-  const to =
     '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>';
+  const to =
+    '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>';
   const basedir = Deno.cwd();
   for (const repoName of getRepos(repoList)) {
     Deno.chdir(`${basedir}/../${repoName}`);
@@ -96,9 +96,9 @@ async function updateBootstrapJs(repoList) {
 
 async function updateBootstrapCss(repoList) {
   const from =
-    '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
-  const to =
     '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">';
+  const to =
+    '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">';
   const basedir = Deno.cwd();
   for (const repoName of getRepos(repoList)) {
     Deno.chdir(`${basedir}/../${repoName}`);
@@ -110,8 +110,8 @@ async function updateBootstrapCss(repoList) {
 }
 
 async function updateBootstrapSwJs(repoList) {
-  const from = "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3";
-  const to = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0";
+  const from = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.0";
+  const to = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.1";
   const files = [
     "src/sw.js",
     "src/ja/sw.js",
@@ -180,7 +180,7 @@ switch (Deno.args[0]) {
     await updateBootstrapSwJs("all.lst");
     await updateServiceWorker("all.lst");
     await build("all.lst");
-    const comment = "bump bootstrap from 5.1.3 to 5.2.0";
+    const comment = "bump bootstrap from 5.2.0 to 5.2.1";
     await $`gitn add .. all.lst "*"`;
     await $`gitn commit .. all.lst -m "${comment}"`;
     await $`gitn push .. all.lst`;
@@ -193,7 +193,7 @@ switch (Deno.args[0]) {
     await build("signature_pad.lst");
     const comment = "bump signature_pad from 4.0.6 to 4.0.7";
     await $`gitn add .. signature_pad.lst "*"`;
-    const result = await $`gitn commit .. signature_pad.lst -m "${comment}"`;
+    await $`gitn commit .. signature_pad.lst -m "${comment}"`;
     await $`gitn push .. signature_pad.lst`;
     break;
   }
