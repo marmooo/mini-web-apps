a6951716198bf29c0d19335724cf92de8b16dda7
marmooo <marmooo@users.noreply.github.com>
2023-05-21 14:11:58 +0900

    bump tfjs from 4.5.0 to 4.6.0

--- update.js
+++ update.js
@@ -69,8 +69,8 @@ async function updateServiceWorker(repoList) {
 }
 
 async function updateTfjs(repoList) {
-  const from = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.4.0";
-  const to = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.5.0";
+  const from = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.5.0";
+  const to = "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.6.0";
   const basedir = Deno.cwd();
   for (const repoName of getRepos(repoList)) {
     Deno.chdir(`${basedir}/../${repoName}`);
@@ -162,7 +162,7 @@ switch (Deno.args[0]) {
     await updateTfjs("tfjs.lst");
     await updateServiceWorker("tfjs.lst");
     await build("tfjs.lst");
-    const comment = "bump tfjs from 4.4.0 to 4.5.0";
+    const comment = "bump tfjs from 4.5.0 to 4.6.0";
     await $`gitn add .. tfjs.lst "*"`;
     await $`gitn commit .. tfjs.lst -m "${comment}"`;
     await $`gitn push .. tfjs.lst`;
