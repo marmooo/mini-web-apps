a18db9d75fdc9a33e7e96dab76b3b7adedd2762b
marmooo <marmooo@users.noreply.github.com>
2025-09-07 11:53:40 +0900

    bump signature_pad from 5.0.10 to 5.1.0

--- update.js
+++ update.js
@@ -80,8 +80,8 @@ async function updateBootstrap(repoList) {
 }
 
 async function updateSignaturePad(repoList) {
-  const from = "signature_pad@5.0.7";
-  const to = "signature_pad@5.0.10";
+  const from = "signature_pad@5.0.10";
+  const to = "signature_pad@5.1.0";
   const basedir = Deno.cwd();
   for (const repoName of getRepos(repoList)) {
     $.cwd = `${basedir}/../${repoName}`;
@@ -135,7 +135,7 @@ switch (Deno.args[0]) {
     await updateSignaturePad("signature_pad.lst");
     await updateServiceWorker("signature_pad.lst");
     await build("signature_pad.lst");
-    const comment = "bump signature_pad from 5.0.7 to 5.0.10";
+    const comment = "bump signature_pad from 5.0.10 to 5.1.0";
     await $`gitn add .. signature_pad.lst -A`.quiet();
     await $`gitn commit .. signature_pad.lst -m ${comment}`.quiet();
     await $`gitn push .. signature_pad.lst`.quiet();
