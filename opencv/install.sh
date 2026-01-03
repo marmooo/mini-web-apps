c027e9f47cc2be18f60f30c264e65626c0118bb8
marmooo <marmooo@users.noreply.github.com>
2026-01-04 00:16:07 +0900

    update opencv deps

--- opencv/install.sh
+++ opencv/install.sh
@@ -1,5 +1,4 @@
 base_dir=${HOME}/workspace
-opencv_dir=${base_dir}/opencv  # 4.11.0
-opencv_contrib_dir=${base_dir}/opencv_contrib  # 4.11.0
-cp embindgen.py ${opencv_dir}/modules/js/generator/
+opencv_dir=${base_dir}/opencv  # 4.13.0
+opencv_contrib_dir=${base_dir}/opencv_contrib  # 4.13.0
 cp -r modules ${opencv_contrib_dir}
